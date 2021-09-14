851f8168e3ac063eb3a456391232bda63d6b2475
Jordane Thomas <thomasjordane7@gmail.com>
2021-09-14 12:59:06 -0400

    modify database seeding script to seed google values

--- seed_empty_database.py
+++ seed_empty_database.py
@@ -3,12 +3,16 @@ from data_access.models import (
     OAuthTokenType as OATT_da,
     MicrosoftScopeUri as MSURI_da,
     MicrosoftScope as MS_da,
+    GoogleScopeUri as GSURI_da,
+    GoogleScope as GS_da,
     SessionFactory as Session,
 )
 from glue import (
     TokenType as OATT_glue,
     MicrosoftScopeURI as MSURI_glue,
     MicrosoftScope as MS_glue,
+    GoogleScopeURI as GSURI_glue,
+    GoogleScope as GS_glue,
 )
 
 
@@ -21,24 +25,43 @@ def seed_empty_database():
             session.add(model)
             session.commit()
 
-        uris = dict()
+        microsoft_uris = dict()
         for uri in MSURI_glue:
             model = MSURI_da(uri=uri.value)
-            uris[uri.value] = model
+            microsoft_uris[uri.value] = model
 
             session.add(model)
             session.commit()
 
-        scopes = dict()
+        microsoft_scopes = dict()
         for scope in MS_glue:
             model = MS_da(name=scope.value)
-            scopes[scope.value] = model
+            microsoft_scopes[scope.value] = model
             for uri in scope.uris:
-                model.uris.append(uris[uri.value])
+                model.uris.append(microsoft_uris[uri.value])
+
+            session.add(model)
+            session.commit()
+
+        google_uris = dict()
+        for uri in GSURI_glue:
+            model = GSURI_da(uri=uri.value)
+            google_uris[uri.value] = model
+
+            session.add(model)
+            session.commit()
+
+        google_scopes = dict()
+        for scope in GS_glue:
+            model = GS_da(name=scope.value)
+            google_scopes[scope.value] = model
+            for uri in scope.uris:
+                model.uris.append(google_uris[uri.value])
 
             session.add(model)
             session.commit()
 
 
 if __name__ == "__main__":
-    seed_empty_database()
+    # seed_empty_database()
+    pass
